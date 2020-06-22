fa768209c24fce5eb0f9e2561d03058ee5a4a8fd
lautisilber <lautisilbergleit@gmail.com>
2020-06-22 17:01:26 -0300

    further offline render development

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def draw_line(pos1, pos2, col):
     dy1 = abs(dy)
     px = 2 * dy1 - dx1
     py = 2 * dx1 - dy1
+
     if dy1 <= dx1:
         if dx >= 0:
             x = x1
@@ -27,7 +28,7 @@ def draw_line(pos1, pos2, col):
 
         draw(x, y, col)
 
-        for i in range(xe):
+        while x < xe:
             x += 1
             if px < 0:
                 px += 2 * dy1
@@ -38,6 +39,31 @@ def draw_line(pos1, pos2, col):
                     y -= 1
                 px += 2 * (dy1 - dx1)
             draw(x, y, col)
+
+    else:
+        if dy >= 0:
+            x = x1
+            y = y1
+            ye = y2
+        else:
+            x = x2
+            y = y2
+            ye = y1
+
+        draw(x, y, col)
+
+        while y < ye:
+            y += 1
+            if py <= 0:
+                py += 2 * dx1
+            else:
+                if (dx < 0 and dy < 0) or (dx > 0 and dy > 0):
+                    x += 1
+                else:
+                    x -= 1
+                py += 2 * (dx1 - dy1)
+            draw(x, c, col)
+
 
 
 def fill_triangle(verts, col):
